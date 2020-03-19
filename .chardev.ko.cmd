74b8009edde999ebcfff9ea0f51edfb867d9de09
Nishantthummar <59600723+Nishantthummar@users.noreply.github.com>
2020-03-19 18:03:43 +0530

    Delete .chardev.ko.cmd

--- .chardev.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/nishant/Training/Linux_Device_Driver/chardev.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/nishant/Training/Linux_Device_Driver/chardev.ko /home/nishant/Training/Linux_Device_Driver/chardev.o /home/nishant/Training/Linux_Device_Driver/chardev.mod.o ;  true
